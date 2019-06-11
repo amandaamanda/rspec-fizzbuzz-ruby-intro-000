7c62eedd167becf0addb1519e7a6a881fd6c5443
Amanda E Nelson <amanda.nelson@yahoo.com>
2019-06-11 19:43:59 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -3,6 +3,6 @@ def fizzbuzz(int)
     puts "Fizz"
   elsif int % 5 == 0
     puts "Buzz"
-    
+
 end# Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
